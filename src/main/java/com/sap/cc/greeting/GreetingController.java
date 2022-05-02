346e03eda1880a4e4f8e0ec70f55226801adb215
Guoquan Xing <guoquan.xing@sap.com>
2022-05-02 11:35:39 +0800

    logging homework from I320247

--- src/main/java/com/sap/cc/greeting/GreetingController.java
+++ src/main/java/com/sap/cc/greeting/GreetingController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import org.slf4j.*;
+
 @RestController
 public class GreetingController {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	private GreetingService service;
 
@@ -17,12 +21,14 @@ public class GreetingController {
 
 	@GetMapping("/hello")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		MDC.put("path", "/hello");
 		return getGreeting("Hello", name);
 	}
 
 	@GetMapping("/howdy")
 	public String deprecatedGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		System.err.println("Deprecated endpoint used.");
+		MDC.put("path", "/howdy");
+		logger.info("Deprecated endpoint used.");
 		return getGreeting("Howdy", name);
 	}
 
@@ -32,6 +38,7 @@ public class GreetingController {
 		} catch (IllegalArgumentException ex) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
 		} finally {
+			MDC.clear();
 		}
 	}
 
--- src/main/java/com/sap/cc/greeting/GreetingService.java
+++ src/main/java/com/sap/cc/greeting/GreetingService.java
@@ -2,13 +2,19 @@ package com.sap.cc.greeting;
 
 import org.springframework.stereotype.Service;
 
+import org.slf4j.*;
+
 @Service
 public class GreetingService {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+
 	public String createGreetingMessage(String greeting, String name) {
 		if (name.matches(".*[0-9].*")) {
 			throw new IllegalArgumentException("Name must not contain numbers!");
 		}
-		System.out.println("created greeting.");
+		logger.debug("created greeting for {}", name);
 		return greeting + " " + name;
 	}
 }
